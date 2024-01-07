cf2ff78be82cc55f2656aa7d426fc0154199230f
Sasha <georgefbrockman@gmail.com>
2024-01-06 18:22:41 -0600

    finishes lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>This is a paragraph element. There is nothing else to be seen here.</p>
+    <img alt="I made this" src={image}/>
+  </div>;
 }
 
 export default About;
